191a482e9d88f2f2f5829e02d4ec3fa880eccee0
alirrah <alir.rah81@gmail.com>
2024-05-24 17:55:34 +0330

    [ADD] create user

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,9 +4,11 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenVerifyView,
 )
+from .views import CreateUser
 
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('register/', CreateUser.as_view(), name='create_user'),
 ]
